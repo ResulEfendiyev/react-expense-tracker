ca65ddfc992d580b582e6816dd12e69ff4bdeee3
ResulEfendiyev <efendiyevresul1@gmail.com>
2020-10-24 08:53:28 +0400

    React Expense Tracker

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div>
+            <h2>
+                Expense tracker
+            </h2>
+        </div>
+    )
+}
